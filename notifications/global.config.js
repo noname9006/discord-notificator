70edb56fed379215ed83e09491ef91206778a8f2
0xKek <noname9006@gmail.com>
2026-03-22 23:49:55 +0400

    Update global notifications with new content and colors

--- notifications/global.config.js
+++ notifications/global.config.js
@@ -8,15 +8,27 @@ module.exports = {
         {
             id: 'global-notification-1',
             embed: new EmbedBuilder()
-                .setColor('Gold')
-                .setTitle('⛓️ Botanix community goes onchain')
+                .setColor('#000000')
+                .setTitle('Botanix Community Goes Onchain!')
                                 .setDescription(
-    'Build your on-chain reputation. Connect wallet, mint your asset.\n\n' +
+    '⛓️ Build your on-chain reputation\n Connect wallet, mint your asset\n\n' +
     '🌐      __**https://ambassador.botanixlabs.com/**__\n\n' +
     'Free mint • Monthly drops • Utility soon\n\n\n' +  // triple \n
     '⚡ Need BTC on Botanix? [Quick bridge guide](https://discord.com/channels/937915188903018498/1186683012004446350/1471173793446760570)'
 )
                 .setImage('https://cdn.discordapp.com/attachments/1430551206115282954/1480524794129809459/NFT_MAG05_CROP.png')
+        },
+		 {
+            id: 'global-notification-2',
+            embed: new EmbedBuilder()
+                .setColor('#000000')
+                .setTitle('🚨 Security Reminder')
+                                .setDescription(
+      "**Spotted a suspicious message?**\n" +
+    "→ Reply directly to that message with `!scam`\n" +
+    "→ We'll handle the rest\n\n" +
+    "🛡️ Your safety is our #1 priority"
+)
         }
     ]
 }
